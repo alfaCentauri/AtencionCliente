74a37b692241c39db880bdc198e6ff82d4008b8c
Ricardo Presilla <ricardopresilla@gmail.com>
2021-11-10 19:01:40 -0400

    Mejoras de la vista y creando metodo ajax

--- assets/app.js
+++ assets/app.js
@@ -48,15 +48,80 @@ var vue = new Vue({
     delimiters: ['${','}$'],
 });
 
+//Source dataSets
+var dataSets = [];
+//DataTables config
+var table = $('#vueTable').DataTable( {
+    data: dataSets,
+    columns: [
+        { title: "idTicket" },
+        { title: "Nombre" },
+    ],
+    "language": {
+        "lengthMenu": "Mostrar _MENU_ registros por página",
+        "zeroRecords": "No hay registros disponibles",
+        "info": "Mostrando página _PAGE_ de _PAGES_",
+        "infoEmpty": "No hay registros disponibles",
+        "infoFiltered": "(filtrado de _MAX_ total registros)",
+        "paginate": {
+            "first": "Primera página",
+            "last": "Ultima página",
+            "next": "Página siguiente",
+            "previous": "Página anterior"
+        },
+        "search": "Buscar: ",
+    },
+    "paging":   false,
+    "ordering": false,
+    "info":     false,
+} );
+var counterRows = 0;
+
+/** Tabla con Vue**/
 var vue2 = new Vue({
     el: '#vue-table',
     data: {
         cola: [],
     },
     delimiters: ['${','}$'],
-    // methods: {
-    //     getCola1() {
-    //         $ajax().
-    //     },
-    // }
+    methods: {
+        getCola1() {
+            $ajax({
+                url: "{{ path('ruta') }}",
+                data: formData,     //debug
+                dataType: 'text',
+                cache: false,
+                async: false,
+                contentType: false,
+                processData: false,
+                mimeType: 'multipart/form_data',
+                type: "POST",
+                success: function (data) {
+                    if (typeof (data) != 'undefined') {
+                        result = JSON.parse(data);
+                        var maximo = result.length;
+                        for(var i = 0; i < maximo; i++){
+                            var node = {
+                                "idTicket": result[i]["idTicket"],
+                                "Nombre": result[i]["name"],
+                            };
+                            dataSets.push(node);
+                            //Update table
+                            table.row.add( [
+                                node['idTicket'],
+                                node['Nombre'],
+                            ] ).draw( false );
+                            counterRows++;
+                            //Clean input
+                    }
+                },
+                error: function () {    //debug
+                    alert("Error on service!");
+                },
+                complete: function () {
+                    console.log("Completado...");
+                }
+            });
+        },
+    }
 });
