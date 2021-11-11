d56a84207ad6543d788f7f17337738b3d9d86207
Ricardo Presilla <ricardopresilla@gmail.com>
2021-11-11 13:10:14 -0400

    Separando la definición de la tabla con Vue en un archivo js.

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,6 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-import $ from 'jquery';
 
 /**Definition of the components */
 Vue.component('id-ticket', {
@@ -47,83 +46,4 @@ var vue = new Vue({
         message: "Nuevo ticket"
     },
     delimiters: ['${','}$'],
-});
-
-//Source dataSets
-var dataSets = [];
-//DataTables config
-var table = $('#vueTable').DataTable( {
-    data: dataSets,
-    columns: [
-        { title: "idTicket" },
-        { title: "Nombre" },
-    ],
-    "language": {
-        "lengthMenu": "Mostrar _MENU_ registros por página",
-        "zeroRecords": "No hay registros disponibles",
-        "info": "Mostrando página _PAGE_ de _PAGES_",
-        "infoEmpty": "No hay registros disponibles",
-        "infoFiltered": "(filtrado de _MAX_ total registros)",
-        "paginate": {
-            "first": "Primera página",
-            "last": "Ultima página",
-            "next": "Página siguiente",
-            "previous": "Página anterior"
-        },
-        "search": "Buscar: ",
-    },
-    "paging":   false,
-    "ordering": false,
-    "info":     false,
-} );
-var counterRows = 0;
-
-/** Tabla con Vue**/
-var vue2 = new Vue({
-    el: '#vue-table',
-    data: {
-        cola: [],
-    },
-    delimiters: ['${','}$'],
-    methods: {
-        getCola1() {
-            $ajax({
-                url: "{{ path('api_colas', {'numeroCola': 1}) }}",
-                data: null,     //debug
-                dataType: 'text',
-                cache: false,
-                async: false,
-                contentType: false,
-                processData: false,
-                mimeType: 'multipart/form_data',
-                type: "POST",
-                success: function (data) {
-                    if (typeof (data) != 'undefined') {
-                        result = JSON.parse(data);
-                        var maximo = result.length;
-                        for (var i = 0; i < maximo; i++) {
-                            var node = {
-                                "idTicket": result[i]["idTicket"],
-                                "Nombre": result[i]["name"],
-                            };
-                            dataSets.push(node);
-                            //Update table
-                            table.row.add([
-                                node['idTicket'],
-                                node['Nombre'],
-                            ]).draw(false);
-                            counterRows++;
-                            //Clean input
-                        }
-                    }
-                },
-                error: function () {    //debug
-                    alert("Error on service!");
-                },
-                complete: function () {
-                    console.log("Completado...");
-                }
-            });
-        },
-    }
 });
