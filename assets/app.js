7642f3859acef2cd0e0a39992d10c4e90f1f8092
Ricardo Presilla <ricardopresilla@gmail.com>
2021-11-12 12:21:22 -0400

    Agregado el controlador para el API.

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,7 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+import $ from 'jquery';
 
 /**Definition of the components */
 Vue.component('id-ticket', {
@@ -37,6 +38,15 @@ Vue.component('name-ticket', {
         '      </div>'
 });
 
+Vue.component('templateRow', {
+    props: ['ticket'],
+    template: '<tr>\n' +
+        '           <td>{{ paginaActual>1?((paginaActual-1)*10)+i+1 : i+1}}</td>\n' +
+        '           <td>${ ticket.idTicket }$</td>\n' +
+        '           <td>${ ticket.nombre }$</td>\n' +
+        '      </tr>'
+});
+
 /**Instancia*/
 var vue = new Vue({
     el: '#vue-app',
@@ -46,4 +56,88 @@ var vue = new Vue({
         message: "Nuevo ticket"
     },
     delimiters: ['${','}$'],
+});
+
+/** Source dataSets */
+var dataSets = [];
+/** DataTables config */
+var table = $('#vueTable').DataTable( {
+    data: dataSets,
+    columns: [
+        { title: "idTicket" },
+        { title: "Nombre" },
+    ],
+    "language": {
+        "lengthMenu": "Mostrar _MENU_ registros por página",
+        "zeroRecords": "No hay registros disponibles",
+        "info": "Mostrando página _PAGE_ de _PAGES_",
+        "infoEmpty": "No hay registros disponibles",
+        "infoFiltered": "(filtrado de _MAX_ total registros)",
+        "paginate": {
+            "first": "Primera página",
+            "last": "Ultima página",
+            "next": "Página siguiente",
+            "previous": "Página anterior"
+        },
+        "search": "Buscar: ",
+    },
+    "paging":   false,
+    "ordering": false,
+    "info":     false,
+} );
+var counterRows = 0;
+
+/** Tabla con Vue **/
+var vue2 = new Vue({
+    el: '#vue-table',
+    data: {
+        tickets: [],
+    },
+    delimiters: ['${','}$'],
+    methods: {
+        getCola(numeroCola, numeroPagina) {
+            $ajax({
+                url: "{{ path('api_cola', {'numeroCola': "+numeroCola+", 'pag': "+numeroPagina+"}) }}",
+                data: null,     //debug
+                dataType: 'text',
+                cache: false,
+                async: false,
+                contentType: false,
+                processData: false,
+                mimeType: 'multipart/form_data',
+                type: "POST",
+                success: function (data) {
+                    if (typeof (data) != 'undefined') {
+                        result = JSON.parse(data);
+                        //
+                        var colaAtencion = result['colaAtencion'];
+                        var totalPaginasCola = result['paginasCola'];
+                        var tiempoEsperatotalCola = result["tiempoEsperaCola"];
+                        //
+                        var maximo = colaAtencion.length;
+                        for (var i = 0; i < maximo; i++) {
+                            var node = {
+                                "idTicket": colaAtencion[i]["idTicket"],
+                                "Nombre": colaAtencion[i]["nombre"],
+                            };
+                            dataSets.push(node);
+                            //Update table
+                            table.row.add([
+                                node['idTicket'],
+                                node['Nombre'],
+                            ]).draw(false);
+                            counterRows++;
+                            //Clean input
+                        }
+                    }
+                },
+                error: function () {    //debug
+                    alert("Error on service!");
+                },
+                complete: function () {
+                    console.log("Completado...");
+                }
+            });
+        },
+    }
 });
